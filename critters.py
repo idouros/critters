46606177df195ca30ed2a8609df72a8088d3866a
Yannis Douros <yannis@douros.org>
2022-03-04 10:03:43 +0200

    Fixed pylance problems

--- critters.py
+++ critters.py
@@ -1,8 +1,8 @@
+"""builds model for identifying between three different species"""
 import os
 
 import argparse
 import configparser
-import csv
 import random
 
 import numpy as np
@@ -17,13 +17,9 @@ def load_dataset(config, sample_size, split_ratio):
     data_location = config['training_data']['location_aligned']
     training_data = config['training_data']['classes'].split(',')
 
-    # Work out the number of classes
-    num_classes = len(training_data)
-    classes = list(range(0, num_classes))
-
     # Initialize the data placeholders
-    X_in = []
-    Y_in = []
+    x_in = []
+    y_in = []
 
     # Load files and pre-process on the fly
     class_id = -1
@@ -40,10 +36,10 @@ def load_dataset(config, sample_size, split_ratio):
             image_resized = np.array(Image.fromarray(image).resize(size=(sample_size, sample_size)))
 
             # Add to set
-            X_in.append(image_resized)
-            Y_in.append(class_id)
+            x_in.append(image_resized)
+            y_in.append(class_id)
 
-    num_input_samples = len(X_in)
+    num_input_samples = len(x_in)
     print("Loaded " + str(num_input_samples) + " images.")
 
     # Shuffle and split into training and test set
@@ -55,22 +51,22 @@ def load_dataset(config, sample_size, split_ratio):
     shuffled_indices = np.arange(0, num_input_samples)
     random.shuffle(shuffled_indices)
 
-    X_train = np.zeros([num_training_set_samples, sample_size, sample_size,3], dtype=int)
-    X_test = np.zeros([num_test_set_samples, sample_size, sample_size, 3], dtype=int)
-    Y_train = np.zeros([num_training_set_samples], dtype=int)
-    Y_test = np.zeros([num_test_set_samples], dtype=int)
+    x_train = np.zeros([num_training_set_samples, sample_size, sample_size,3], dtype=int)
+    x_test = np.zeros([num_test_set_samples, sample_size, sample_size, 3], dtype=int)
+    y_train = np.zeros([num_training_set_samples], dtype=int)
+    y_test = np.zeros([num_test_set_samples], dtype=int)
 
     for j in range (0, num_input_samples):
         if j < num_training_set_samples:
-            X_train[j] = X_in[shuffled_indices[j]]/255.
-            Y_train[j] = Y_in[shuffled_indices[j]]
+            x_train[j] = x_in[shuffled_indices[j]]/255.
+            y_train[j] = y_in[shuffled_indices[j]]
         else:
-            X_test[j-num_training_set_samples] = X_in[shuffled_indices[j]]/255.
-            Y_test[j-num_training_set_samples] = Y_in[shuffled_indices[j]]
+            x_test[j-num_training_set_samples] = x_in[shuffled_indices[j]]/255.
+            y_test[j-num_training_set_samples] = y_in[shuffled_indices[j]]
 
  #   print(Y_train[:5])
-    return X_train, Y_train, X_test, Y_test, classes
-    
+    return x_train, y_train, x_test, y_test
+
 def load_and_preprocess(image_file_name, sample_size):
     """ load and preprocess"""
     image = np.array(plt.imread(image_file_name))
@@ -82,7 +78,7 @@ def main():
     """main"""
     print("TensorFlow version:", tf.__version__)
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--train', dest='config_file',  help='Load the specified config file and train the NN')
+    parser.add_argument('-t', '--train', dest='config_file',  help='config file')
     args = parser.parse_args()
 
     # Data loading
@@ -91,7 +87,7 @@ def main():
     sample_size = config.getint('training_data', 'sample_size', fallback = '64')
     split_ratio = config.getfloat('training_data', 'split_ratio', fallback = '0.8')
     class_labels = config.get('training_data', 'classes').split(",")
-    X_train, Y_train, X_test, Y_test, classes = load_dataset(config, sample_size, split_ratio)
+    x_train, y_train, x_test, y_test = load_dataset(config, sample_size, split_ratio)
 
     model = tf.keras.models.Sequential([
         tf.keras.layers.Flatten(input_shape=(sample_size, sample_size, 3)),
@@ -114,11 +110,10 @@ def main():
         loss=loss_fn,
         metrics=['accuracy']
         )
-    model.fit(X_train, Y_train, epochs=5)
-    model.evaluate(X_test,  Y_test, verbose=2)
+    model.fit(x_train, y_train, epochs=5)
+    model.evaluate(x_test,  y_test, verbose=2)
     probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
 
-    
 #    predictions = model(X_train[:5]).numpy()
 #    print(predictions)
 #    print(tf.nn.softmax(predictions).numpy())
@@ -130,7 +125,7 @@ def main():
                 "Data/Aligned/squirrel/squirrel_0000.jpg",
                 "IMG_20201025_101839.jpg"
                 ]
-    
+
     for fname in fnames:
         test_image = load_and_preprocess(fname, sample_size)
         test_image = np.expand_dims(test_image, axis=0)
@@ -138,11 +133,11 @@ def main():
         confidence = np.max(predictions)
         predicted_class_id = np.argmax(predictions)
         print(predictions)
-        print("Prediction: {} is an image depicting: {}, with {:2.2f}% confidence".format(
+        print("Prediction: {} is an image depicting: {}, with {:2.2f}% confidence",
             fname,
             class_labels[predicted_class_id],
             confidence*100.0
-            ))
+            )
 
 if __name__ == "__main__":
     main()
